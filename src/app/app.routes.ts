9a65929f00f77a80a5ef01ffa652ea34ec55847d
Abdelrahman Abdeen <abdelrahman.abden@sociumtech.com>
2025-03-26 23:07:31 +0300

    some fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
 import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
-import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: BodyComponent },
   { path: 'users/add', component: FormComponent },
   { path: 'users/edit/:id', component: FormComponent },
-  { path: 'users/delete/:id', component: DeleteDialogComponent},
   { path: '**', redirectTo: '' }
 ];
